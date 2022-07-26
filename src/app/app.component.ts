0f576e23b80d010bfdeeec697e61dae900be0094
huynh <lqh.nuchaku@gmail.com>
2022-07-27 00:55:07 +0700

    push all code

---
+++ src/app/app.component.ts
@@ -0,0 +1,45 @@
+import { Component } from '@angular/core';
+import {finalize, Observable} from "rxjs";
+import {AngularFireStorage} from "@angular/fire/compat/storage";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = "cloudsSorage";
+  // @ts-ignore
+  selectedFile: File = null;
+  // @ts-ignore
+  fb;
+  // @ts-ignore
+  downloadURL: Observable<string>;
+  constructor(private storage: AngularFireStorage) {}
+  ngOnInit() {}
+
+  // @ts-ignore
+  onFileSelected(event) {
+    let n = Date.now();
+    const file = event.target.files[0];
+    const filePath = `RoomsImages/${n}`;
+    const fileRef = this.storage.ref(filePath);
+    const task = this.storage.upload(`RoomsImages/${n}`, file);
+    task.snapshotChanges().pipe(
+        finalize(() => {
+          this.downloadURL = fileRef.getDownloadURL();
+          this.downloadURL.subscribe(url => {
+            if (url) {
+              this.fb = url;
+            }
+            console.log(this.fb);
+          });
+        })
+      )
+      .subscribe(url => {
+        if (url) {
+          console.log(url);
+        }
+      });
+  }
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyCsOCwcab6VidEFpuWJNTiBlmmy870Ke-A",
+    authDomain: "upload-image-947da.firebaseapp.com",
+    databaseURL: "https://upload-image-947da-default-rtdb.firebaseio.com",
+    projectId: "upload-image-947da",
+    storageBucket: "upload-image-947da.appspot.com",
+    messagingSenderId: "19125480067",
+    appId: "1:19125480067:web:dde36a8b052df6d491c9b6",
+    measurementId: "G-WBEM0CE4VB"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
